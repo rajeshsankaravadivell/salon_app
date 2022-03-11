6c8dfbc2e32db7cb230b75353b722ab510d452ef
vetrivelu <vetrivelu25@gmail.com>
2022-03-11 16:29:58 +0530

    initiate test comiit

---
+++ lib/screens/settings/account setup/profile_setup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileSetupForm extends StatelessWidget {
+  const ProfileSetupForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
